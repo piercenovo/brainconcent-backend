472899935a0c783166a16b15dad39cf4348472d2
piercenovo <piercenovoah@gmail.com>
2022-06-04 17:59:22 -0500

    feat: add validate user fields

---
+++ middlewares/validate-fields.js
@@ -0,0 +1,18 @@
+const { validationResult } = require('express-validator')
+
+const validateFields = (req, res, next) => {
+  const err = validationResult(req)
+
+  if (!err.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: err.mapped()
+    })
+  }
+
+  next()
+}
+
+module.exports = {
+  validateFields
+}
--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,17 @@
 */
 
 const { Router } = require('express')
+const { check } = require('express-validator')
 const { createUser } = require('../controllers/auth')
+const { validateFields } = require('../middlewares/validate-fields')
 
 const router = Router()
 
-router.post('/new', createUser)
+router.post('/new', [
+  check('name', 'El nombre es obligatorio').not().isEmpty(),
+  check('email', 'El correo es obligatorio').isEmail(),
+  check('password', 'La contraseña es obligatoria').not().isEmpty(),
+  validateFields
+], createUser)
 
 module.exports = router
