e843f3955773c0809cc267e4497fc6fd0c118a03
piercenovo <piercenovoah@gmail.com>
2022-10-27 14:44:07 -0500

    add change genera.config.js v2

--- src/config/general.config.js
+++ src/config/general.config.js
@@ -3,5 +3,5 @@ config()
 
 export const PORT = process.env.PORT || 8000
 export const HOST = process.env.HOST || 'localhost'
-export const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://piercenovo:localtabla778@cluster1.hakav.mongodb.net/brainDB'
-export const JWT_KEY = process.env.JWT_KEY || 'FR@NV3262694!!-E/commers..%$$%&/"#$/#!!"%&elt$/ndu'
+export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/brainDB'
+export const JWT_KEY = process.env.JWT_KEY || '~#@ASDASDA_aSDLPAOsauasss~$ccc{sa'
