e3b2c20310a51693ed54a539e03a5f08fa49ed35
piercenovo <piercenovoah@gmail.com>
2022-10-27 14:46:05 -0500

    add change general.config.js v3

--- src/config/general.config.js
+++ src/config/general.config.js
@@ -4,4 +4,4 @@ config()
 export const PORT = process.env.PORT || 8000
 export const HOST = process.env.HOST || 'localhost'
 export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/brainDB'
-export const JWT_KEY = process.env.JWT_KEY || '~#@ASDASDA_aSDLPAOsauasss~$ccc{sa'
+export const JWT_KEY = process.env.JWT_KEY || '~#@ASDASDA_aSDLPAOsauaccc{sa'
